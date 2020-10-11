2b6a4ee3de7b17546e462e173e2afeb4a08e2b07
missval <56055461+missval@users.noreply.github.com>
2020-10-11 14:07:26 -0400

    Updates to the R code

--- Logistic regression for a binomial sentiment variable.R
+++ Logistic regression for a binomial sentiment variable.R
@@ -1,52 +1,41 @@
 #Load the required libraries
-
 library(caTools)
 library(ROCR)
 library(tm)
 library(SnowballC)
 library(rpart)
 library(rpart.plot)
+library(tidyverse)
+
+# Part 1---------------------------------------------------------------------------------------------
 
 #read in csv file
+rmp=read.csv(file=”RateMyProfessor_Sample data.csv)
 
-rmp=read.csv(file=”ProfessorRatings.csv)
-
-#subset the data that contains non-missing observations for would_take_agains
-
-rmp_log=na.omit(rmp, cols="would_take_agains")
+#subset the data that contains non-missing observations for student_star the binary would_take_agains
+rmp=drop_na(rmp, "student_star")
+rmp=drop_na(rmp, "would_take_agains")
 
 #create a sentiment variable
-
 rmp_log$sentiment=ifelse(rmp_log$would_take_agains=="Yes", 1, 0)
 
 #split the data into training and testing 70/30
-
-split=sample.split(rmp_log$sentiment, SplitRatio=0.7)
-
-train=subset(rmp_log, split==TRUE)
-
-test=subset(rmp_log, split==FALSE)
+split1=sample.split(rmp_log$sentiment, SplitRatio=0.7)
+train1=subset(rmp_log, split1==TRUE)
+test1=subset(rmp_log, split1==FALSE)
 
 #train a logistic regression model
-
-log_model=glm(sentiment~student_star, data=train, family="binomial")
+log_model1=glm(sentiment~student_star, data=train1, family="binomial")
 
 #make predictions on a test data
-
-predictions_log=predict(log_model, newdata=test, type="response")
+predictions_log1=predict(log_model1, newdata=test1, type="response")
 
 #assess the accuracy of the model
-
-table(test$sentiment, as.numeric(predictions_log>=0.5))
+table(test1$sentiment, as.numeric(predictions_log1>=0.5))
 
 #assess the accuracy of the baseline model
+table(test1$sentiment)
 
-table(test$sentiment)
-
-#plot and calculate AUC curve
-
-pred=prediction(predictions_log, test$sentiment)
-
-as.numeric(performance(pred, "auc")@y.values)
-
-print(log_model)
+#calculate AUC value
+pred1=prediction(predictions_log1, test1$sentiment)
+as.numeric(performance(pred1, "auc")@y.values)
--- Logistic regression for text analysis.R
+++ Logistic regression for text analysis.R
@@ -1,25 +1,28 @@
 #Load the required libraries
-
 library(caTools)
 library(ROCR)
 library(tm)
 library(SnowballC)
 library(rpart)
 library(rpart.plot)
+library(tidyverse)
+
+# Part 1---------------------------------------------------------------------------------------------
 
 #read in csv file
-rmp=read.csv(file=”ProfessorRatings.csv)
+rmp=read.csv(file=”RateMyProfessor_Sample data.csv)
 
-#subset the data that contains non-missing observations for the binary would_take_agains
-rmp_log=na.omit(rmp, cols="would_take_agains")
+#subset the data that contains non-missing observations for student_star the binary would_take_agains
+rmp=drop_na(rmp, "student_star")
+rmp=drop_na(rmp, "would_take_agains")
 
 #create a sentiment variable
 rmp_log$sentiment=ifelse(rmp_log$would_take_agains=="Yes", 1, 0)
 
 #split the data into training and testing 70/30
-split=sample.split(rmp_log$sentiment, SplitRatio=0.7)
-train1=subset(rmp_log, split==TRUE)
-test1=subset(rmp_log, split==FALSE)
+split1=sample.split(rmp_log$sentiment, SplitRatio=0.7)
+train1=subset(rmp_log, split1==TRUE)
+test1=subset(rmp_log, split1==FALSE)
 
 #train a logistic regression model
 log_model1=glm(sentiment~student_star, data=train1, family="binomial")
@@ -33,18 +36,17 @@ table(test1$sentiment, as.numeric(predictions_log1>=0.5))
 #assess the accuracy of the baseline model
 table(test1$sentiment)
 
-#plot and calculate AUC curve
+#calculate AUC value
 pred1=prediction(predictions_log1, test1$sentiment)
 as.numeric(performance(pred1, "auc")@y.values)
-print(log_model1)
 
-#----------------------------------------------------------
+# Part 2--------------------------------------------------------------------------------------------
 
 #subset the data with only missing observations for the binary would_take_agains
-rmp_na=rmp[rowSums(is.na(rmp))>0,]
+rmp_na=rmp[is.na(rmp$would_take_agains),]
 
 #use the trained logistic model to make predictions on the dataset with missing would_take_agains 
-predictions_na=predict(log_model, newdata=rmp_na, type="response")
+predictions_na=predict(log_model1, newdata=rmp_na, type="response")
 
 #create a sentiment variable
 rmp_na$sentiment=ifelse(predictions_na>=0.5, 1, 0)
@@ -67,9 +69,9 @@ labeledComments=as.data.frame(as.matrix(dtm))
 labeledComments$sentiment=rmp_final$sentiment
 
 #split the data into training and testing 70/30
-split=sample.split(labeledComments$sentiment, SplitRatio=0.7)
-train2=subset(labeledComments, split==TRUE)
-test2=subset(labeledComments, split==FALSE)
+split2=sample.split(labeledComments$sentiment, SplitRatio=0.7)
+train2=subset(labeledComments, split2==TRUE)
+test2=subset(labeledComments, split2==FALSE)
 
 #train a logistic regression model on the vectorized dataset of student's comments
 log_model2=glm(sentiment~., data=train2, family="binomial")
@@ -78,6 +80,10 @@ log_model2=glm(sentiment~., data=train2, family="binomial")
 predictions_log2=predict(log_model2, newdata=test2, type="response")
 table(test2$sentiment, as.numeric(predictions_log2>=0.5))
 table(test2$sentiment)
+
+#calculate AUC value
+pred2=prediction(predictions_log2, test2$sentiment)
+as.numeric(performance(pred2, "auc")@y.values)
 
 #create new variable "approval rate" for each instructor
 rmp_final$approval_rate=mean(rmp_final$sentiment)
